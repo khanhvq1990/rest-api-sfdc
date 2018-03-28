bc911e67bcbb2974bde57548f10201db94e1ee9f
khanhvq1990 <32129099+khanhvq1990@users.noreply.github.com>
2018-03-28 14:13:39 +0900

    Update app.js

--- client/js/app.js
+++ client/js/app.js
@@ -1,33 +1,35 @@
-function getSessionList(success, error) {
-	var soql = "SELECT Session__r.Id, Session__r.Name FROM Session_Speaker__c";
+function getCapmaignList(success, error) {
+    var soql = "SELECT id, Name FROM Campaign";
     force.query(soql, success, error);
 }
 
-function getSessionDetails(sessionId, success, error) {
-	var soql = "SELECT Session__r.Name, " +
-        "Session__r.Session_Date__c, " +
-        "Speaker__r.First_Name__c, " +
-        "Speaker__r.Last_Name__c " +
-        "FROM Session_Speaker__c " +
-        "WHERE Session__r.Id = '" + sessionId + "'";
+function getCampaignDetails(campaignId, success, error) {
+	
+    var soql = "SELECT Name, Type, NumberOfLeads, NumberOfContacts FROM Campaign WHERE Id='" + campaignId + "'";
     force.query(soql, success, error);
 }
 
-function showSessionList() {
-    getSessionList(
+function getCampaignMembers(campaignId, success, error) {
+    var soql = "SELECT Contact.Email, Lead.Email FROM CampaignMember WHERE CampaignId='" + campaignId + "' and Contact.Email <> ''";
+    force.query(soql, success, error);
+}
+
+function showCampaignList() {
+	console.log('##############################');
+    getCapmaignList(
         function (data) {
-            var sessions = data.records,
+            var campaigns= data.records,
                 html = '';
-            for (var i=0; i<sessions.length; i++) {
-                html += '<li class="table-view-cell"><a href="#sessions/'+ sessions[i].Session__r.Id +'">' + sessions[i].Session__r.Name + '</a></li>';
+            for (var i=0; i<campaigns.length; i++) {
+                html += '<li class="table-view-cell"><a href="#campaign/'+ campaigns[i].Id +'">' + campaigns[i].Name + '</a></li>';
             }
             html =
                 '<div class="page">' +
                 '<header class="bar bar-nav">' +
-                    '<h1 class="title">Sessions</h1>' +
+                    '<h1 class="title">Campaigns</h1>' +
                 '</header>' +
                 '<div class="content">' +
-                    '<ul class="table-view session-list">' + html + '</ul>' +
+                    '<ul class="table-view campaign-list">' + html + '</ul>' +
                 '</div>' +
                 '</div>';
             slider.slidePage($(html));
@@ -38,29 +40,32 @@ function showSessionList() {
     return false;
 }
 
-function showSessionDetails(sessionId) {
+function showCampaignDetails(campaignId) {
 
-    getSessionDetails(sessionId,
+    getCampaignDetails(campaignId,
         function (data) {
-            var session = data.records[0],
+            var campaign= data.records[0],
             html =
                 '<div class="page">' +
                 '<header class="bar bar-nav">' +
                 '<a class="btn btn-link btn-nav pull-left" href="#"><span class="icon icon-left-nav"></span>Back</a>' +
-            '<h1 class="title">Sessions</h1>' +
+                '<a class="btn btn-link btn-nav pull-right" href="#campaignmembers/' + campaignId +'"><span class="icon icon-right-nav"></span>Show Members</a>' +
+            '<h1 class="title">Selected Campaign</h1>' +
                 '</header>' +
                 '<div class="content">' +
                     '<div class="card">' +
                         '<ul class="table-view">' +
                             '<li class="table-view-cell">' +
-                                '<h4>' + session.Session__r.Name + '</h4>' +
-                                '<p>' + (session.Session__r.Session_Date__c || 'No time yet')+ '</p>' +
+                                '<h4>' + campaign.Name + '</h4>' +
                             '</li>' +
-                            '<li class="table-view-cell">Speaker: ' +
-                                session.Speaker__r.First_Name__c +
+                            '<li class="table-view-cell">キャンペーン種別: ' +
+                                campaign.Type +
                             '</li>' +
-                            '<li class="table-view-cell">' +
-                                (session.Session__r.Description__c || 'No description yet') +
+                            '<!--<li class="table-view-cell">リード数: ' +
+                                campaign.NumberOfLeads +
+                            '</li>-->' +
+                            '<li class="table-view-cell">担当者数: ' +
+                                campaign.NumberOfContacts +
                             '</li>' +
                         '</ul>' +
                     '</div>' +
@@ -74,6 +79,36 @@ function showSessionDetails(sessionId) {
     return false;
 }
 
+function showCampaignMembers(campaignId) {
+
+    getCampaignMembers(campaignId,
+        function (data) {
+            var cMembers= data.records,
+            html = "";
+            for (var i=0; i<cMembers.length; i++) {
+                html += '<li class="table-view-cell">' + cMembers[i].Contact.Email+ '</li>';
+            }
+            html =
+                '<div class="page">' +
+                '<header class="bar bar-nav">' +
+                '<a class="btn btn-link btn-nav pull-left" href="#"><span class="icon icon-left-nav"></span>Back</a>' +
+                '<a class="btn btn-link btn-nav pull-right" href="#sendContacts/' + campaignId +'"><span class="icon icon-right-nav"></span>Send Contacts Mail</a>' +
+                    '<h1 class="title">Contacts Mail</h1>' +
+                '</header>' +
+                '<div class="content">' +
+                    '<ul class="table-view campaign-list">' + html + '</ul>' +
+                '</div>' +
+                '</div>';
+
+            slider.slidePage($(html));
+        },
+        function (error) {
+            alert("Error: " + JSON.stringify(error));
+        });
+    return false;
+}
+
 var slider = new PageSlider($('body')); // Initialize PageSlider micro-library for nice and hardware-accelerated page transitions
-router.addRoute('', showSessionList);
-router.addRoute('sessions/:id', showSessionDetails);
+router.addRoute('', showCampaignList);
+router.addRoute('campaign/:id', showCampaignDetails);
+router.addRoute('campaignmembers/:id', showCampaignMembers);
